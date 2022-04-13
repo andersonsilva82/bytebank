4d9606ff9deaa5e0565c41d66147a22e7f2499cf
Anderson Silva <ander.loirao@gmail.com>
2022-04-13 16:40:43 -0300

    Aula 07

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transferências'),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: const Text('Transferências'),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-    ),
-  ));
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.00,1)),
+        ItemTransferencia(Transferencia(200.00,1)),
+        ItemTransferencia(Transferencia(300.00,2)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _tranferencia;
+
+  const ItemTransferencia(this._tranferencia, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_tranferencia.valor.toString()),
+      subtitle: Text(_tranferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
