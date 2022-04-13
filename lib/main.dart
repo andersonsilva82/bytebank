981d8e597ff19f6b66571afb8570522b6f2b5961
Anderson Silva <ander.loirao@gmail.com>
2022-04-13 14:46:03 -0300

    Commit Inicial

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    ),
+  ));
+}
